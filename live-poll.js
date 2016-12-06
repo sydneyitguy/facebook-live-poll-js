6e987ce2573f1636cfdb74452daa23f09d9cbfac
Sebastian Kim <sydneyitguy@gmail.com>
2016-12-06 10:35:02 +0900

    Import JS file into HTML

--- live-poll.js
+++ live-poll.js
@@ -1,8 +1,8 @@
-var LivePoll = (function() {
+window.LivePoll = (function() {
 
   // Only variables to configure
-  var ACCESS_TOKEN = '';
-  var VIDEO_ID = '';
+  var ACCESS_TOKEN = '1234567890|WHATEVERblahblah';
+  var VIDEO_ID = '1234567890';
 
   getUrl = function(reactionType) {
     return 'https://graph.facebook.com/' + VIDEO_ID + '/reactions?type=' + reactionType +'&limit=0&summary=true&access_token=' + ACCESS_TOKEN;
